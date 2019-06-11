8cd90e9436fc09ea478a40f00987cc3bb95a6b0f
Hannah Frost <h.7frost@gmail.com>
2019-06-11 14:55:06 +0100

    User can see the temperature at 8am
    Co-authored-by: Ayelisha Amoah <ayelisha.amoah@gmail.com>

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        isLoading: true,
+        dataSource: null,
+      }
+  }
+
+  componentDidMount() {
+    return fetch('https://samples.openweathermap.org/data/2.5/forecast/hourly?id=524901&appid=b6907d289e10d714a6e88b30761fae22')
+      .then ( (response) => response.json() )
+      .then( (responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.list,
+        })
+      })
+    .catch((error) => {
+      console.log(error)
+    });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator />
+        </View>
+      )
+    } else {
+      let temp = this.state.dataSource.slice(0, 24).map(i => {
+        if (i.dt_txt.includes("08:00:00")) {
+          return (
+            <View item={i} key={i.id} style={styles.container}>
+              <Text>{i.main.temp}</Text>
+            </View>
+          )
+        }
+      })
+      return (
+        <View style={styles.container}>
+          {temp}
+        </View>
+      )
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +60,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     fontFamily: 'Verdana',
     fontSize: 20,
+  },
+  item: {
+    flex: 1,
+    alignSelf: 'stretch',
   },
 });
