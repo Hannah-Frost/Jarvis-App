a4b4868911a84d6b940535fd38d4c1af10e1de9c
Alasdair <alasdair123@hotmail.co.uk>
2019-06-17 10:56:33 +0100

    commit 1

--- app/components/TravelTime.js
+++ app/components/TravelTime.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default class TravelTime extends Component {
   constructor() {
-    super()
-    this.getLocationAsync = this.getLocationAsync.bind(this)
-    this.getDestinationAsync = this.getDestinationAsync.bind(this)
-    this.getTravelTimeAsync = this.getTravelTimeAsync.bind(this)
+    super();
+    this.getLocationAsync = this.getLocationAsync.bind(this);
+    this.getDestinationAsync = this.getDestinationAsync.bind(this);
+    this.getTravelTimeAsync = this.getTravelTimeAsync.bind(this);
     this.state = {
       currentLocation: null,
       destination: null,
@@ -20,7 +20,7 @@ export default class TravelTime extends Component {
     let { status } = await Expo.Permissions.askAsync(Expo.Permissions.LOCATION);
     if (status !== "granted") {
       this.setState({
-        errorMessage: 'Permission to access location was denied',
+        errorMessage: "Permission to access location was denied"
       });
     }
     let currentLocation = await Expo.Location.getCurrentPositionAsync({});
@@ -28,38 +28,40 @@ export default class TravelTime extends Component {
     let currentCoords = {
       lat: currentLocation.coords.latitude,
       long: currentLocation.coords.longitude
-    }
+    };
     this.setState({ currentLocation, currentCoords });
   };
 
   getDestinationAsync = async () => {
-    console.log(this.props.postcode)
+    console.log(this.props.postcode);
     let destination = await Expo.Location.geocodeAsync(this.props.postcode);
 
     let destinationCoords = {
       lat: destination[0].latitude,
       long: destination[0].longitude
-    }
+    };
     this.setState({ destination, destinationCoords });
   };
 
   getTravelTimeAsync(currentLat, currentLong, destLat, destLong) {
     // let travelTime = await fetch ('https://developer.citymapper.com/api/1/traveltime/?startcoord=+51.558079%2C-0.120810&endcoord=51.558823%2C-0.121487&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key=e2567df81c88213608d82487879b3f96')
     // fetch (`https://developer.citymapper.com/api/1/traveltime/?startcoord=51.51727169201791%2C-0.07331195425420282&endcoord=51.5583838%2C-0.123121&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key=e2567df81c88213608d82487879b3f96`)
-    fetch (`https://developer.citymapper.com/api/1/traveltime/?startcoord=${currentLat}%2C${currentLong}&endcoord=${destLat}%2C${destLong}&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key=e2567df81c88213608d82487879b3f96`)
-    .then( (response) => response.json() )
-    .then( (responseJson) => {
-      this.setState({
-        dataSource: responseJson.travel_time_minutes,
+    fetch(
+      `https://developer.citymapper.com/api/1/traveltime/?startcoord=${currentLat}%2C${currentLong}&endcoord=${destLat}%2C${destLong}&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key=e2567df81c88213608d82487879b3f96`
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          dataSource: responseJson.travel_time_minutes
+        });
       })
-    })
-    .catch((error) => {
-      console.log(error)
-    });
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   updateDestination = () => {
-    this.getDestinationAsync().then(() => {;
+    this.getDestinationAsync().then(() => {
       this.getLocationAsync().then(() => {
         this.getTravelTimeAsync(
           this.state.currentCoords.lat,
@@ -69,26 +71,25 @@ export default class TravelTime extends Component {
         );
       });
     });
-  }
-  
+  };
+
   componentDidUpdate(prevProps, prevState) {
-  // only update chart if the data has changed
-  if (prevProps.postcode !== this.props.postcode) {
-    this.updateDestination()
+    // only update chart if the data has changed
+    if (prevProps.postcode !== this.props.postcode) {
+      this.updateDestination();
+    }
   }
-}
 
   render() {
-    var journeyTime = this.state.dataSource
-    if (this.props.postcode != '' && journeyTime != null) {
+    var journeyTime = this.state.dataSource;
+    if (this.props.postcode != "" && journeyTime != null) {
       return (
-        <Text>Today's commute: {journeyTime} minutes to {this.props.postcode}</Text>
-      )
-    }
-    else {
-      return (
-      <Text>Please enter your postcode</Text>
-    )
+        <Text>
+          Today's commute: {journeyTime} minutes to {this.props.postcode}
+        </Text>
+      );
+    } else {
+      return <Text>Please enter your postcode</Text>;
     }
   }
 }
