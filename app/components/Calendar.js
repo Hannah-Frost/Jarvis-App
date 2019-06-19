6cdc08126ab52e7fa56a174b3a501322649841fc
Alasdair <alasdair123@hotmail.co.uk>
2019-06-19 16:39:02 +0100

    fix time format

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -10,17 +10,8 @@ export default class CalendarPull extends Component {
     this.state = {
       errorMessage: null,
       localCalendars: null,
-<<<<<<< HEAD
-<<<<<<< HEAD
       events: null,
       eventDetails: {}
-=======
-      events: null
->>>>>>> add calendar component, pulls all calendars and assigns ids
-=======
-      events: null,
-      eventDetails: {}
->>>>>>> calendar event dtails acvailable in calendar render fixed
     };
   }
 
@@ -31,8 +22,6 @@ export default class CalendarPull extends Component {
         errorMessage: "Permission to access calendar was denied"
       });
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
     let localCalendars = await Expo.Calendar.getCalendarsAsync();
     let calIDs = [];
     localCalendars.map(calendar => {
@@ -41,10 +30,6 @@ export default class CalendarPull extends Component {
 
     this.setState({ localCalendars, calIDs });
   };
-=======
-
-    let localCalendars = await Expo.Calendar.getCalendarsAsync();
->>>>>>> calendar api working
 
   getCalendarEventsAsync = async () => {
     let today = new Date();
@@ -53,62 +38,29 @@ export default class CalendarPull extends Component {
       new Date(),
       new Date(today.getTime() + 24 * 60 * 60 * 1000)
     );
-    var eventDetails = {
-      eventTitle: events[0].title,
-      eventStartTime: events[0].startDate.replace(
-        /^[^:]*([01]\d:[01]\d).*$/,
-        "$1"
-      ),
-      eventEndTime: events[0].endDate.replace(/^[^:]*([01]\d:[01]\d).*$/, "$1"),
-      eventLocation: events[0].location
-    };
-    this.setState({ events, eventDetails }, () => {
-      this.props.storeEventDetails(eventDetails);
-    });
-=======
-    let localCalendars = await Expo.Calendar.getCalendarsAsync("event");
-    let calendarIDs = {
-      id1: localCalendars[0].id,
-      id2: localCalendars[1].id,
-      id3: localCalendars[2].id,
-      id4: localCalendars[3].id
-    };
-    this.setState({ localCalendars, calendarIDs });
-  };
 
-  getCalendarEventsAsync = async () => {
-    let events = await Expo.Calendar.getEventsAsync(
-      [
-        this.state.calendarIDs.id1,
-        this.state.calendarIDs.id2,
-        this.state.calendarIDs.id3,
-        this.state.calendarIDs.id4
-      ],
-      new Date("2019-06-18"),
-      new Date("2019-06-19")
-    );
-<<<<<<< HEAD
-<<<<<<< HEAD
-    this.setState({ events: myEvents });
-    console.log(this.state.myEvents);
->>>>>>> add calendar component, pulls all calendars and assigns ids
-=======
-    let eventDetails = {
-=======
+    function ConvertNumberToTwoDigitString(n) {
+      return n > 9 ? "" + n : "0" + n;
+    }
+    let start = new Date(events[0].startDate);
+    let end = new Date(events[0].endDate);
     var eventDetails = {
->>>>>>> eventDetails available in app.js confirmed line 188
       eventTitle: events[0].title,
-      eventStartTime: events[0].startDate.replace(
-        /^[^:]*([01]\d:[01]\d).*$/,
-        "$1"
-      ),
-      eventEndTime: events[0].endDate.replace(/^[^:]*([01]\d:[01]\d).*$/, "$1"),
+      eventStartTime: `${ConvertNumberToTwoDigitString(
+        start.getUTCHours()
+      )}:${ConvertNumberToTwoDigitString(start.getUTCMinutes())}`,
+      eventEndTime: `${ConvertNumberToTwoDigitString(
+        end.getUTCHours()
+      )}:${ConvertNumberToTwoDigitString(end.getUTCMinutes())}`,
       eventLocation: events[0].location
     };
+
+    console.log(eventDetails.eventStartTime);
+    console.log(eventDetails.eventEndTime);
+
     this.setState({ events, eventDetails }, () => {
       this.props.storeEventDetails(eventDetails);
     });
->>>>>>> retrieve first event of day and details, format time
   };
 
   componentDidMount() {
@@ -118,11 +70,6 @@ export default class CalendarPull extends Component {
   }
 
   render() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     if (this.state.eventDetails.eventTitle != null) {
       return (
         <View>
@@ -140,33 +87,5 @@ export default class CalendarPull extends Component {
           <Text>No appointments today</Text>
         </View>
       );
-=======
-    return <Text>{this.state.events}</Text>;
->>>>>>> add calendar component, pulls all calendars and assigns ids
-=======
-    return <Text>{JSON.stringify(this.state.eventDetails)}</Text>;
->>>>>>> retrieve first event of day and details, format time
-=======
-    var eventDetails = this.state.eventDetails;
-    return <Text>{JSON.stringify(eventDetails)}</Text>;
->>>>>>> eventDetails available in app.js confirmed line 188
-=======
-    console.log(this.state.eventDetails);
-    var eventDetails = this.state.eventDetails;
-    return <Text>{this.state.eventDetails.eventTitle}</Text>;
->>>>>>> calendar event dtails acvailable in calendar render fixed
-=======
-    var eventDetails = this.state.eventDetails;
-    return (
-      <View>
-        <Text>
-          Today's first appointment: {this.state.eventDetails.eventTitle}
-        </Text>
-        <Text>Location: {this.state.eventDetails.eventLocation}</Text>
-        <Text>Starts: {this.state.eventDetails.eventStartTime}</Text>
-        <Text>Ends: {this.state.eventDetails.eventEndTime}</Text>
-      </View>
-    );
->>>>>>> calednar wrking
   }
 }
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -210,6 +210,9 @@ export default class HomeScreen extends React.Component {
                 storeTravelTime={this.storeTravelTime}
               />
             </View>
+            <View>
+              <CalendarPull storeEventDetails={this.storeEventDetails} />
+            </View>
           </View>
         </LinearGradient>
       );
